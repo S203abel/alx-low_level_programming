fbf213800ad5ef68313aee0e4cec4e45cf56f54e
S203abel <gebruabel7@gmail.com>
2023-08-12 07:31:45 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < height;i ++)
+	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
 
