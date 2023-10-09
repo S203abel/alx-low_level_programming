fefd9b2de65db7cb9dedba7509b3b56f79a75b22
S203abel <gebruabel7@gmail.com>
2023-10-09 05:28:20 -0700

    update

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,6 @@
+#ifndef PI_H
+#define PI_H
+
 #define SIZE 1024
+
+#endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-int main()
+/**
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
+int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
 
